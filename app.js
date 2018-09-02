a1c7b9e084c3b3d23bdc092518445e0337e68506
Daniel Diaz <dd0871421@otc.edu>
2018-09-01 21:43:17 -0500

    Finished GIT Some Practice Assignment

---
+++ app.js
@@ -0,0 +1,18 @@
+//Presentaion Display Name and Age
+console.log("Daniel Diaz");
+console.log("My age is: " + 24);
+//Minimalistic Calculation
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+//Values Prediction
+    //9
+    //string '4 + 5'
+    //string Concatination 45
+console.log(4 + 5);
+console.log("4 + 5");
+console.log("4" + "5"); 
+//Play with Variables From Celsius to Fahrenheit
+let cel = 32; 
+console.log("32 degrees celius is " + (cel*9 /5+32) + " Degrees Fahrenheit" );
